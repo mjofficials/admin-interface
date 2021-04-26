37a1a91b52ca6608dd8ce7df729986abe2a51809
mjofficials <manojmarimuthu1995@gmail.com>
2021-04-26 19:47:34 +0530

    bugfixes and added dynamic buttons on addstock page

--- src/components/AddStocksPage/Tags/Tags.js
+++ src/components/AddStocksPage/Tags/Tags.js
@@ -1,10 +1,48 @@
-import React from "react";
-import TagChain from "./TagChain";
+import React, { useState } from "react";
+import ToolTipBtn from "../../HelperComponents/ToolTipBtn/ToolTipBtn";
+
+// const btnArr = ['United States', 'Small cap', 'Phase3', 'PFIC', 'Biliary atresia', 'Alagille syndrome', 'Phase3', 'PFIC', 'Biliary atresia', 'Alagille syndrome']
+// const btnArr = ['United States']
+// const btnArr = [];
 
 const Tags = () => {
+  const [value, setValue] = useState([]);
+  const [changeText, setChangeText] = useState([]);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+    // console.log(e.target.value);
+  };
+  const changeTextHandler = () => {
+    setChangeText([...changeText, { value, id: Math.random() * 100 }]);
+    setValue("");
+  };
+
   return (
     <div>
-      <TagChain />
+      {/* <TagChain userValue={value} /> */}
+      <div className="input-group mb-3">
+        <label className="input-group-text" htmlFor="inputGroupSelect01">
+          Tag Chain
+        </label>
+        <select className="form-select" id="inputGroupSelect01">
+          <option value="">Onchology</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+      </div>
+      <div className="">
+        {changeText.map((text) => (
+          <ToolTipBtn
+            changeText={changeText}
+            setChangeText={setChangeText}
+            btnName={text.value}
+            key={text.id}
+            text={text}
+          />
+        ))}
+      </div>
       <div className="input-group my-3">
         <label className="input-group-text" htmlFor="inputGroupSelect01">
           Add Tag
@@ -15,8 +53,15 @@ const Tags = () => {
           placeholder=""
           aria-label=""
           aria-describedby=""
+          value={value}
+          onChange={handleChange}
         />
-        <button className="btn btn-primary" type="button" id="button-addon1">
+        <button
+          className="btn btn-primary"
+          type="button"
+          id="button-addon1"
+          onClick={changeTextHandler}
+        >
           Add Tag
         </button>
       </div>
--- src/components/HelperComponents/ToolTipBtn/ToolTipBtn.js
+++ src/components/HelperComponents/ToolTipBtn/ToolTipBtn.js
@@ -2,10 +2,19 @@ import React from "react";
 // eslint-disable-next-line no-unused-vars
 import ToolTipStyles from "./ToolTipBtn.module.css";
 
-const ToolTipBtn = ({ btnName }) => {
+const ToolTipBtn = ({ btnName, changeText, setChangeText, text }) => {
+  const deleteHandler = () => {
+    // console.log(text);
+    setChangeText(changeText.filter((el) => el.id !== text.id));
+  };
+
   return (
     <>
-      <button className={`ToolTipStyles btn btn-primary m-1`} data-tooltip="Top">
+      <button
+        onClick={deleteHandler}
+        className={`ToolTipStyles btn btn-primary m-1`}
+        data-tooltip="Top"
+      >
         {btnName}
       </button>
     </>
